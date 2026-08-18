d913024dfd3613590be7ef43e883029ff038ce9f
nasirsyasin <nsywattoo@gmail.com>
2026-08-18 18:15:33 +0500

    yml and login test files added for Playwright testing framework.

---
+++ tests/login.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://practicetestautomation.com/');
+  await page.getByRole('button', { name: 'Close' }).click();
+  await page.getByRole('link', { name: 'Practice', exact: true }).click();
+  await page.getByRole('link', { name: 'Test Login Page' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('student');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Password123');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByRole('heading', { name: 'Logged In Successfully' }).click();
+  await page.getByRole('link', { name: 'Log out' }).click();
+});
