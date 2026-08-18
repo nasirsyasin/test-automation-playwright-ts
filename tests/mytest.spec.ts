45602893b8d6aa806d97ddf93faf06e2462cf4ce
nasirsyasin <nsywattoo@gmail.com>
2026-08-18 18:35:33 +0500

    files revamped

--- tests/mytest.spec.ts
+++
@@ -1,9 +0,0 @@
-import {test, expect} from '@playwright/test';
-
-test('verify pagetitle', async ({page}) => {
-    await page.goto('https://www.google.com');
-    
-    let title: string = await page.title();
-        console.log(title);
-    await expect(page).toHaveTitle('Google');
-})
--- tests/mytest2.spec.ts
+++
@@ -1,9 +0,0 @@
-import {test, expect} from '@playwright/test';
-
-test('verify pageURL', async ({page}) => {
-    await page.goto('https://www.testo.com/it-IT');
-    
-    let url: string = await page.url();
-        console.log('URL:', url);
-    await expect(page).toHaveURL(/testo/);
-})
